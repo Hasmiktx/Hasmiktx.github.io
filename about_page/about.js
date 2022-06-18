802601013564034fbace5f86e97f2e45dad17f8f
Hasmiktx <hasmiktx@gmail.com>
2022-06-18 18:42:36 +0400

    added portpolio

--- about_page/about.js
+++ about_page/about.js
@@ -14,8 +14,8 @@ function skillFunc() {
   conteiner1.style.textAlign = "center";
   conteiner1.style.fontSize = "30px";
   conteiner1.style.margin = "30px";
-
-  conteiner1.style.backgroundImage = "url('images/pic2.jpg')";
+  conteiner1.style.backgroundImage =
+    "url('https://img.rawpixel.com/private/static/images/website/2022-05/p-465-td-7164.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=3cc6b165be9bad0313de3804a8dfc9bb')";
 }
 
 skills.addEventListener("click", skillFunc);
