cb18330d2a2335eaf53142889b1a2cb2f5981d02
Hasmiktx <hasmiktx@gmail.com>
2023-12-15 23:00:36 +0400

    logic changes

--- Circle/main.js
+++ Circle/main.js
@@ -1,11 +1,9 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width=window.innerWidth/1.5
 canvas.height=window.innerHeight;
-const circles=[];
-
 const backgImg=document.createElement("img");
 backgImg.src="gravity.jpg"
+const circles=[];
 
 
 
@@ -15,7 +13,7 @@ backgImg.src="gravity.jpg"
     this.y=y,
     this.r=getRandom(20,50),
     this.yDelta=0;
-    this.color="rgb("+getRandom(0,255)+","+getRandom(0,255)+","+getRandom(0,255)+")";
+    this.color=`rgb(${getRandom(0,255)}, ${getRandom(0,255)}, ${getRandom(0,255)})`
     this.update=function(deltaTime){
           
         this.yDelta += 0.5 * deltaTime;
@@ -24,31 +22,37 @@ backgImg.src="gravity.jpg"
 
         if (this.y + this.r> canvas.height) {
             this.y = canvas.height - this.r;
-            this.yDelta *= -0.8; // Dampening effect on bounce
+            this.yDelta *= -0.8; 
         }
 
     }
     this.draw=function(){
-       if(this.x<10){
-        this.x=this.x+this.r
-       }else if(canvas.width-this.x<15){
-        this.x=canvas.width-this.r
-       }
+        if(this.x<this.r){
+            this.x=this.x+this.r
+        }else if(canvas.width-this.x<this.r){
+            this.x=canvas.width-this.r
+        }
+       
        ctx.beginPath();
        ctx.arc(this.x,this.y, this.r, 0, 2 * Math.PI);
        ctx.fillStyle = this.color;
        ctx.fill();
     
-    //    console.log(this.color,"color")
+    
     }
    }
+
+
+
 const draw=()=>{
+    canvas.width  = window.innerWidth/1.4;
+
     ctx.clearRect(0,0,canvas.width,canvas.height)
     ctx.drawImage(backgImg, 0, 0, canvas.width, canvas.height);
-  circles.forEach((ball)=>{
-    
-    ball.draw()
-  })  
+     circles.forEach((ball)=>{
+        ball.draw()
+    }
+    )  
 
 }
 
@@ -69,17 +73,19 @@ function tick() {
   
   tick();
   
-
-   function getRandom(min,max){
+//get random  numbers
+function getRandom(min,max){
     return Math.floor(Math.random()*(max-min))+min
    }
-  document.addEventListener("click",(evt)=>{
+
+
+document.addEventListener("click",(evt)=>{
     
     const ball=new Circle(evt.clientX,evt.clientY);
     
-    if(circles.length>=15){
+     if(circles.length>=15){
       circles.shift();
-    }
-    circles.push(ball)
+     }
+      circles.push(ball)
 
-  })
+})
